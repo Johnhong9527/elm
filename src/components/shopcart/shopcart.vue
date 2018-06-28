16395a6bfc755d1a1cc03f6b4075097ea3b115d8
seamong <1310512423@qq.com>
2018-06-28 11:19:38 +0800

    shopcart

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,17 +3,18 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
           </div>
-          <div class="num">{{totalCount}}</div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">￥{{totalPrice}}元</div>
-        <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+        <div class="price" :class="{'highlight':totalCount>0}">￥{{totalPrice}}元</div>
+        <div class="desc">{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥{{minPrice}}元起送
+        <div class="pay" :class="payClass">
+        <!-- <div class="pay" :class="{'enough':this.totalPrice >= this.minPrice}"> -->
+          {{payDesc}}
         </div>
       </div>
     </div>
@@ -54,6 +55,21 @@ export default {
         conut += food.count;
       });
       return conut;
+    },
+    payDesc() {
+      if (this.totalPrice === 0) {
+        return `￥${this.minPrice}元`;
+      } else if (this.totalPrice < this.minPrice) {
+        let diff = this.minPrice - this.totalPrice;
+        return `还差￥${diff}元起送`;
+      }
+      return "去结算";
+    },
+    payClass() {
+      if (this.totalPrice < this.minPrice) {
+        return "not-enough";
+      }
+      return "enough";
     }
   }
 };
@@ -92,20 +108,33 @@ export default {
           border-radius: 50%;
           background: rgb(43, 52, 60);
           text-align: center;
+          &.highlight {
+            background-color: rgb(0, 160, 220);
+          }
           .icon-shopping_cart {
             .lh(44);
             .fs(24);
             color: #80858a;
+            &.highlight {
+              color: #fff;
+            }
           }
         }
-        .num{
+        .num {
           position: absolute;
           top: 0;
           right: 0;
           .w(24);
           .h(16);
-          .lh(16);
+          .lh(17.5);
+          vertical-align: text-bottom;
           text-align: center;
+          .br(16);
+          .fs(9);
+          font-weight: 700;
+          color: white;
+          background-color: rgb(240, 20, 20);
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
         }
       }
       .price {
@@ -113,12 +142,16 @@ export default {
         vertical-align: top;
         .mt(12);
         .lh(24);
+        line-height: 24px;
         .pr(12);
         box-sizing: border-box;
         .setRightLine(rgba(255,255,255,0.1));
         .fs(16);
         font-weight: 700;
         color: rgba(255, 255, 255, 0.4);
+        &.highlight {
+          color: #fff;
+        }
       }
       .desc {
         display: inline-block;
@@ -139,7 +172,13 @@ export default {
         .fs(12);
         color: rgba(255, 255, 255, 0.4);
         font-weight: 700;
-        background-color: #2b333b;
+        &.not-enough {
+          background-color: #2b333b;
+        }
+        &.enough {
+          background-color: #00b43c;
+          color: #fff;
+        }
       }
     }
   }
